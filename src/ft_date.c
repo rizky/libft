aee8d42b7d01198214ebede773a6f7b3dd5cd8f0
rizkyario <rizkyario@yahoo.com>
2018-02-27 13:37:44 +0100

    Normed

--- src/ft_date.c
+++ src/ft_date.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_date.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rnugroho <rnugroho@students.42.fr>         +#+  +:+       +#+        */
+/*   By: rnugroho <rnugroho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/23 16:50:22 by rnugroho          #+#    #+#             */
-/*   Updated: 2018/02/24 20:48:25 by rnugroho         ###   ########.fr       */
+/*   Updated: 2018/02/27 13:36:56 by rnugroho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +24,10 @@ static const long g_days_per_year[2] = {
 long long
 	ft_datetoepoch(long date[6])
 {
-	long long epoch;
-	long years;
-	long leap_years;
-	int		i;
+	long long	epoch;
+	long		years;
+	long		leap_years;
+	int			i;
 
 	years = 0;
 	leap_years = 0;
@@ -35,14 +35,15 @@ long long
 	while (++i < date[5])
 		if (IS_LEAP_YEAR(i))
 			leap_years++;
-		else 
+		else
 			years++;
-	epoch = ((years*g_days_per_year[0]) + (leap_years*g_days_per_year[1])) * SEC_PER_DAY;
-	long year_index = (IS_LEAP_YEAR( date[5] )) ? 1 : 0;
+	epoch = ((years * g_days_per_year[0]) + (leap_years * g_days_per_year[1]));
+	epoch = epoch * SEC_PER_DAY;
+	years = (IS_LEAP_YEAR(date[5])) ? 1 : 0;
 	i = -1;
-	while (++i < date[4]-1)
-		epoch += g_days_per_month[year_index][i] * SEC_PER_DAY;
-	epoch += (date[3]-1) * SEC_PER_DAY + date[2] * SEC_PER_HOUR
+	while (++i < date[4] - 1)
+		epoch += g_days_per_month[years][i] * SEC_PER_DAY;
+	epoch += (date[3] - 1) * SEC_PER_DAY + date[2] * SEC_PER_HOUR
 	+ date[1] * SEC_PER_MIN + date[0];
 	return (epoch);
 }
@@ -50,30 +51,28 @@ long long
 long
 	*ft_epochtodate(long long epoch)
 {
-	long *date;
-	long long dayclock, dayno;
-	long year;
+	long		*date;
+	long long	dayno;
+	long		year;
 
 	date = (long *)malloc(sizeof(long) * 6);
-	dayclock =  epoch % SEC_PER_DAY;
 	dayno = epoch / SEC_PER_DAY;
-
-	date[0] = dayclock % 60;
-	date[1] = (dayclock % 3600) / 60;
-	date[2] = dayclock / 3600;
+	date[0] = epoch % SEC_PER_DAY % 60;
+	date[1] = (epoch % SEC_PER_DAY % 3600) / 60;
+	date[2] = epoch % SEC_PER_DAY / 3600;
 	year = EPOCH_YEAR;
-	while (dayno >= YEARSIZE(year)) 
+	while (dayno >= YEARSIZE(year))
 	{
-			dayno -= YEARSIZE(year);
-			year++;
+		dayno -= YEARSIZE(year);
+		year++;
 	}
 	date[5] = year;
 	date[4] = 0;
-    while (dayno >= g_days_per_month[IS_LEAP_YEAR(year)][date[4]])
-    {
-        dayno -= g_days_per_month[IS_LEAP_YEAR(year)][date[4]];
-        date[4]++;
-    }
+	while (dayno >= g_days_per_month[IS_LEAP_YEAR(year)][date[4]])
+	{
+		dayno -= g_days_per_month[IS_LEAP_YEAR(year)][date[4]];
+		date[4]++;
+	}
 	date[3] = dayno + 1;
-	return date;
+	return (date);
 }
