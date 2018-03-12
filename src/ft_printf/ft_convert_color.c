46ecfbffbe7a6d6138e123bfb4d5a90d41a87539
rizkyario <rizkyario@yahoo.com>
2018-03-12 20:08:54 +0100

    Fixed leaks at pf_cv_w

--- src/ft_printf/ft_convert_color.c
+++ src/ft_printf/ft_convert_color.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_convert_color.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rnugroho <rnugroho@students.42.fr>         +#+  +:+       +#+        */
+/*   By: rnugroho <rnugroho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/18 22:27:14 by rnugroho          #+#    #+#             */
-/*   Updated: 2018/03/01 00:44:39 by rnugroho         ###   ########.fr       */
+/*   Updated: 2018/03/12 20:07:55 by rnugroho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ int
 	pf_cv_w(t_modifier *m, t_array *d, va_list ap)
 {
 	int		index;
+	char	*value;
 
 	(void)ap;
 	(void)m;
@@ -27,8 +28,10 @@ int
 		index += 30;
 	fta_append(d, "\e[", 2);
 	fta_append(d, "0;", 2);
-	fta_append(d, ft_itoa(index), ft_strlen(ft_itoa(index)));
+	value = ft_itoa(index);
+	fta_append(d, value, ft_strlen(value));
 	fta_append(d, "m", 1);
+	free(value);
 	return (1);
 }
 
