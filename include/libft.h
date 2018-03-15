0dce2a97a829bf08efbeeacf44f0ca08f45bb640
Rizky Ario <rizkyario@yahoo.com>
2018-03-15 17:52:44 +0100

    Added ft_atol && ft_atoll

--- include/libft.h
+++ include/libft.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rnugroho <rnugroho@students.42.fr>         +#+  +:+       +#+        */
+/*   By: rnugroho <rnugroho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/17 17:38:44 by rnugroho          #+#    #+#             */
-/*   Updated: 2018/03/13 02:24:20 by rnugroho         ###   ########.fr       */
+/*   Updated: 2018/03/15 17:52:27 by rnugroho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,8 @@ typedef struct	s_list
 t_list			*ft_create_elem(void *data);
 
 int				ft_atoi(const char *str);
+int				ft_atol(const char *str);
+int				ft_atoll(const char *str);
 void			ft_bzero(void *s, size_t n);
 int				ft_isalnum(int c);
 int				ft_isalpha(int c);
