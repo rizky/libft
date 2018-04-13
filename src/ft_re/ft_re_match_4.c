2c399f56cd7130f179b35aaddd1ac5b468374887
Rizky Ario <rizkyario@yahoo.com>
2018-04-13 16:57:55 +0200

    Fixed ft_re_match_capture

--- src/ft_re/ft_re_match_4.c
+++ src/ft_re/ft_re_match_4.c
@@ -6,12 +6,16 @@
 /*   By: rnugroho <rnugroho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/13 15:41:46 by rnugroho          #+#    #+#             */
-/*   Updated: 2018/04/13 15:43:34 by rnugroho         ###   ########.fr       */
+/*   Updated: 2018/04/13 16:56:28 by rnugroho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_re.h"
 #include "libft.h"
+
+/*
+** To do: Improve protection if $ at the end
+*/
 
 char
 	*ft_re_match_capture(const char *pattern, const char *group, const char *text)
@@ -19,16 +23,22 @@ char
 	int 	len;
 	int		start;
 	char 	*str;
+	char	*temp;
 
 	if (ft_re_match(pattern, text) == -1)
 		return NULL;
 	start = ft_re_match(group, text);
 	text = text + start;
 	len = ft_strlen(text);
-	while (ft_re_matchn(group, text, len - 1) == 0)
+	temp = ft_memalloc(ft_strlen(group) + 2);
+	ft_memcpy(temp, group, ft_strlen(group));
+	temp[ft_strlen(group)] = '$';
+	temp[ft_strlen(group) + 1] = '\0';	
+	while (ft_re_matchn(temp, text, len) != 0)
 		len--;
-	str = ft_memalloc(len + 1 - 2);
-	ft_memcpy(str, text + 1, len - 2);
+	free(temp);
+	str = ft_memalloc(len + 1);
+	ft_memcpy(str, text, len);
 	str[len] = '\0';
 	return (str);
 }
