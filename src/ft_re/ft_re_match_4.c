30ba8e1f3f8ae952114d3878758bcf78d45227df
Rizky Ario <rizkyario@yahoo.com>
2018-04-16 09:29:30 +0200

    Added better protection at ft_re

--- src/ft_re/ft_re_match_4.c
+++ src/ft_re/ft_re_match_4.c
@@ -6,40 +6,21 @@
 /*   By: rnugroho <rnugroho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/13 15:41:46 by rnugroho          #+#    #+#             */
-/*   Updated: 2018/04/15 16:31:31 by rnugroho         ###   ########.fr       */
+/*   Updated: 2018/04/16 09:28:36 by rnugroho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_re.h"
 #include "libft.h"
 
-/*
-** To do: Improve protection if $ at the end
-*/
-
 char
 	*ft_re_match_capture(const char *pattern, const char *group, const char *text)
 {
-	int 	len;
-	int		start;
 	char 	*str;
-	char	*temp;
 
 	if (ft_re_match(pattern, text) == -1)
 		return NULL;
-	start = ft_re_match(group, text);
-	text = text + start;
-	len = ft_strlen(text);
-	temp = ft_memalloc(ft_strlen(group) + 2);
-	ft_memcpy(temp, group, ft_strlen(group));
-	temp[ft_strlen(group)] = '$';
-	temp[ft_strlen(group) + 1] = '\0';
-	while (len > 0 && ft_re_matchn(temp, text, len) != 0)
-		len--;
-	free(temp);
-	str = ft_memalloc(len + 1);
-	ft_memcpy(str, text, len);
-	str[len] = '\0';
+	str = ft_re_capture(group, text);
 	return (str);
 }
 
@@ -54,6 +35,8 @@ char
 	if (text == NULL)
 		return (NULL);
 	start = ft_re_match(group, text);
+	if (start == -1)
+		return (NULL);
 	text = text + start;
 	len = ft_strlen(text);
 	temp = ft_memalloc(ft_strlen(group) + 2);
